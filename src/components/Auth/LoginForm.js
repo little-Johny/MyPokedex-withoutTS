78b73587507ccca677fbdd29734a48656eb802dc
Johny Molano <molano.johny721@gmail.com>
2025-03-16 19:05:04 -0500

    feat(form): add Formik and Yup in LoginForm, for validation and management of the form

--- src/components/Auth/LoginForm.js
+++ src/components/Auth/LoginForm.js
@@ -7,8 +7,18 @@ import {
     Keyboard, 
 } from 'react-native';
 import React from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 export default function LoginForm() {
+
+    const formik = useFormik({
+        initialValues: initialValues(),
+        validationSchema: Yup.object(validationSchema()),
+        validateOnChange: false,
+        onSubmit: (data) => console.log(`Sent form`, data),
+    });
+
     return (
         <View>
             <Text style={styles.title}>Sing  In</Text>
@@ -16,14 +26,20 @@ export default function LoginForm() {
                 placeholder='Username'
                 style={styles.input}
                 autoCapitalize='none'
+                value={formik.values.username}
+                onChangeText={(text) => formik.setFieldValue('username',text)}
             />
             <TextInput
                 placeholder='password'
                 style={styles.input}
                 autoCapitalize='none'
-                secureTextEntry={true}    
+                secureTextEntry={true}
+                value={formik.values.password}
+                onChangeText={(text) => formik.setFieldValue('password',text)}    
             />
-            <Button title='Enter' onPress={() => console.log('Login')}/>
+            <Button title='Enter' onPress={formik.handleSubmit}/>
+            <Text style={styles.error}>{formik.errors.username}</Text>
+            <Text style={styles.error}>{formik.errors.password}</Text>
         </View>
     )
 }
@@ -36,11 +52,26 @@ const styles = StyleSheet.create({
         marginTop: 50,
         marginBottom: 15,
     },
-    input : {
+    input: {
         height: 40,
         margin: 12,
         borderWidth: 1,
         padding: 10,
         borderRadius: 10,
+    },
+    error: {
+        textAlign: 'center',
+        color: 'red',
+        marginTop:20,
     }
+});
+
+const initialValues = () => ({
+    username: '',
+    password: '',
+});
+
+const validationSchema = () => ({
+    username: Yup.string().required('Username is required'),
+    password: Yup.string().required('Password is required')
 });
